b5b5e256b4dd5aa0b359a0c9266fb700810f9425
Tanushri Barsainya <125864601+tanushri1506@users.noreply.github.com>
2024-05-31 19:27:04 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/Justice");
+mongoose.connect("mongodb://localhost:27017/Justice");
 
 app.post('/appointment',async(req,res)=>{
     // JusticeModel.create(req.body)
